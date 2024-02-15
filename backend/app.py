7ff24761e319794c006fe4631d30f219477a39fd
Hazel Hilbert <hazelhilbert@gmail.com>
2024-02-15 14:40:06 +0000

    refactored backend and nlp folders

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import APITools
+
+app = Flask(__name__)
+
+my_key = "480c76cff050a40771e1190b3cab219d"
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+@app.route("/billText/<bill_id>")
+def getText(bill_id):
+    return APITools.getTextFromID(str(bill_id))
---
+++ backend/free_nlp_api_on_example.py
@@ -0,0 +1,52 @@
+# NOTE: Ensure you've got your OpenAI key in your env variables
+# If on Windows setx OPENAI_API_KEY "your-api-key-here"
+# If on macOS export OPENAI_API_KEY='your-api-key-here'
+# I assume you use export on Linux too but the OpenAI docs don't specify this.
+# Make sure you run   pip install openai
+import os
+
+from openai import OpenAI
+client = OpenAI()
+
+current_directory = os.getcwd()
+filename = current_directory + "/file"
+
+# Options: summary, citationJSON, citationString. Default prompt is 'summary'
+type = "summary"
+
+# Open and read filename.txt and place the content of the file into the string called string
+with open(filename+'.txt', 'r') as file:
+    string = file.read().rstrip()
+
+# Truncate string to fit ChatGPT's token restriction
+string = string[0:2500]
+
+# Choose what prompt you want
+def get_prompt(type):
+  if (type == "summary"):
+   prompt = "Summarize this text for me: "
+  elif (type == "citationJSON"):
+    prompt = "Return ONLY the citation(s) from this string as json: "
+  elif (type == "citationString"):
+    prompt = "Return ONLY the citation(s) from this string: "
+  else:
+    prompt = "Summarize this text for me: "
+  return prompt
+
+# Call ChatGPT
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": get_prompt(type)+string}
+  ]
+)
+
+# Print the prompt given to ChatGPT
+# print("Prompt: "+get_prompt(type)+string+"\n")
+
+response = completion.choices[0].message.content
+
+print(response)
+
+# return response
