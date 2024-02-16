73326470ca33831bc0d1a06b4a3dc54fc19f8527
Tom Gilbride <tom.gilbride8@gmail.com>
2024-02-16 22:25:42 +0000

    Remove folders from branch

--- frontend_nlp/node_modules/tedious/node_modules/@azure/msal-browser/dist/packageMetadata.js
+++
@@ -1,8 +0,0 @@
-/*! @azure/msal-browser v2.38.3 2023-10-27 */
-'use strict';
-/* eslint-disable header/header */
-var name = "@azure/msal-browser";
-var version = "2.38.3";
-
-export { name, version };
-//# sourceMappingURL=packageMetadata.js.map
--- frontend_nlp/node_modules/tedious/node_modules/@azure/msal-browser/node_modules/@azure/msal-common/dist/packageMetadata.js
+++
@@ -1,8 +0,0 @@
-/*! @azure/msal-common v13.3.1 2023-10-27 */
-'use strict';
-/* eslint-disable header/header */
-var name = "@azure/msal-common";
-var version = "13.3.1";
-
-export { name, version };
-//# sourceMappingURL=packageMetadata.js.map
