25c9e2c39d840f10e10029cfb37c24cbb2183a66
Tom Gilbride <tom.gilbride8@gmail.com>
2024-02-16 15:42:37 +0000

    Summarise Button now functioning, can summarise a Bill or selected text

--- frontend_nlp/src/summarise/summarise.js
+++ frontend_nlp/src/summarise/summarise.js
@@ -1,44 +1,50 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global document, Office, Word */
-
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("displaySelectedText").onclick = displaySelectedText; 
-    document.getElementById("displayAllText").onclick = displayAllText;
   }
 });
 
 async function displaySelectedText() {
-  return Word.run(async (context) => {
-    const range = context.document.getSelection();
-    range.load("text");
-    await context.sync();
-    // Set the selected text inside the selectedText div
-    document.getElementById("selectedText").innerText = range.text;
-  }).catch(function (error) {
-    console.log("Error: " + JSON.stringify(error));
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
-  });
+    return Word.run(async (context) => {
+        const range = context.document.getSelection();
+        range.load("text");
+        await context.sync();
+        const selectedText = range.text;
+        await summariseText(selectedText);
+
+    }).catch(function (error) {
+        console.log("Error: " + JSON.stringify(error));
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    });
 }
 
-async function displayAllText() {
-  return Word.run(async (context) => {
-    const body = context.document.body;
-    body.load("text");
-    await context.sync();
-    document.getElementById("selectedText").innerText = body.text;
-  }).catch(function (error) {
-    console.log("Error: " + JSON.stringify(error));
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+document.getElementById("searchButton").addEventListener("click", performSearch);
+document.getElementById("searchInput").addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+        performSearch();
     }
-  });
+});
+
+async function performSearch() {
+    var searchText = document.getElementById("searchInput").value;
+    fetch("http://127.0.0.1:5000/billText/" + searchText)
+        .then(async response => await response.text())
+        .then(data => summariseText(data))
+        .catch(error => console.error('Error:', error));
+}
+
+async function summariseText(text) {
+    fetch("http://127.0.0.1:5000/summariseText/" + text)
+        .then(async response => await response.text())
+        .then(data => displayOutput(data))
+        .catch(error => console.error('Error:', error));
+}
+
+function displayOutput(text) {
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = "<p>" + text + "</p>";
 }
