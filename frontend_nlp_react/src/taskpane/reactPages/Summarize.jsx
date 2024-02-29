4afac843eb5326385ddc4a3fa5f7ffa8cfb7327a
MauvG <muaviaijazghazi@gmail.com>
2024-02-29 13:10:33 +0000

    Moved all styles into a css file from summarize

--- frontend_nlp_react/src/taskpane/reactPages/Summarize.jsx
+++ frontend_nlp_react/src/taskpane/reactPages/Summarize.jsx
@@ -1,33 +1,12 @@
-import * as React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
-import { makeStyles } from "@fluentui/react-components";
-import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
-import { EditArrowBack24Regular, DocumentOnePageMultiple24Regular } from '@fluentui/react-icons';
 import { Input } from "@fluentui/react-components";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
-import { createRoot } from "react-dom/client";
-
-
-const useStyles = makeStyles({
-  root: {
-    minHeight: "100vh",
-  },
-});
-
-const globalStyles = {
-  color: '#707070',
-  fontSize: 14,
-  fontFamily: 'Segoe UI, sans-serif',
-  fontWeight: 400,
-  lineHeight: 20,
-  wordWrap: 'break-word',
-};
-
+import { EditArrowBack24Regular, DocumentOnePageMultiple24Regular } from "@fluentui/react-icons";
+import "../css/Summarize.css";
 
 const Summarize = () => {
-  const styles = useStyles();
-
   const [summarizedText, setSummarizedText] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -56,7 +35,7 @@ const Summarize = () => {
   };
 
   const handleSearchInputChange = (event) => {
-    setSearchQuery(event.target.value); 
+    setSearchQuery(event.target.value);
   };
 
   const handleKeyPress = (event) => {
@@ -74,55 +53,54 @@ const Summarize = () => {
 
   return (
     <FluentProvider theme={webLightTheme}>
-      <div style={{globalStyles, width: '100%', height: '100%', background: 'white', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 14, display: 'inline-flex'}}>
+      <div className="mainContainer globalStyles">
         {/* Propylon Logo */}
         <div className="image">
-          <img src="../../assets/propylonFull.png" alt="Propylon Logo" style={{ maxWidth: '100%', width: 128, height: 'auto' }}/>
+          <img src="../../assets/propylonFull.png" alt="Propylon Logo" />
         </div>
         {/* Search Bar */}
-        <div style={{ width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 14, display: 'inline-flex' }}>
-          <div style={{ alignSelf: 'stretch', height: 32, borderRadius: 4, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex' }}>
-            <div style={{ alignSelf: 'stretch', paddingLeft: 10, paddingRight: 10, background: 'rgba(255, 255, 255, 0)', justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'inline-flex' }}>
-              <div style={{ flex: '1 1 0', height: 32, justifyContent: 'flex-start', alignItems: 'center', display: 'flex' }}>
-                <Input appearance="underline" style={{flex: '1 1 0', height: 32, paddingTop: 5, paddingBottom: 7, paddingLeft: 2, paddingRight: 2, justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex', color: '#707070', fontSize: 14, fontFamily: 'Segoe UI', fontWeight: '400', wordWrap: 'break-word' }} placeholder="Enter Bill ID" value={searchQuery} onChange={handleSearchInputChange} onKeyPress={handleKeyPress} />
-              </div>
+        <div className="searchBarContainer">
+          <div className="searchInputContainer">
+            <div className="searchInputWrapper">
+              <Input
+                appearance="underline"
+                className="searchInput globalStyles"
+                placeholder="Enter Bill ID"
+                value={searchQuery}
+                onChange={handleSearchInputChange}
+                onKeyPress={handleKeyPress}
+              />
             </div>
-            <div style={{ width: 321, height: 1, background: '#575757' }} />
+            <div className="underline"></div>
           </div>
         </div>
         {/* Summary */}
-        <div style={{alignSelf: 'stretch', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
-            <div style={{paddingLeft: 7, paddingRight: 7, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 14, display: 'inline-flex'}}>
-            <button onClick={handleSummarize} className="summarizeButton" style={{ alignSelf: 'stretch', color: '#212529', backgroundColor: '#f8f9fa', border: '1px solid #dee2e6', borderRadius: '0.2rem', fontSize: '14px', fontFamily: 'Segoe UI', fontWeight: '600', padding: '0.375rem 0.75rem', cursor: 'pointer' }}>
+        <div className="summaryContainer">
+          <div>
+            <button onClick={handleSummarize} className="summaryButton">
               Summarize Text
             </button>
-            </div>
-            <div style={{paddingLeft: 14, paddingRight: 14, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 11, display: 'flex'}}>
-                <button  style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
-                  <EditArrowBack24Regular style={{width: 16, height: 16, position: 'relative'}}> </EditArrowBack24Regular>
-                </button>
-                <button  style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
-                  <DocumentOnePageMultiple24Regular style={{width: 16, height: 16, position: 'relative'}}> </DocumentOnePageMultiple24Regular>
-                </button>
-            </div>
-        </div>
-        <div style={{height: 120, paddingLeft: 7, paddingRight: 7, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 14, display: 'flex'}}>
-          <div className="line" style={{alignSelf: 'stretch', color: 'black', fontSize: 14, fontFamily: 'Segoe UI', fontWeight: '400', wordWrap: 'break-word'}}>
-            <p>{summarizedText}</p>
           </div>
+          <div>
+            <button className="iconButton">
+              <EditArrowBack24Regular className="icon"></EditArrowBack24Regular>
+            </button>
+            <button className="iconButton">
+              <DocumentOnePageMultiple24Regular className="icon"></DocumentOnePageMultiple24Regular>
+            </button>
+          </div>
+        </div>
+        <div className="line">
+          <p>{summarizedText}</p>
         </div>
       </div>
     </FluentProvider>
   );
 };
 
-const summarizeTitle = "Propylon Legislation summarize";
-// const summarizeRootElement = document.getElementById("summarize-root");
-// const summarizeRoot = createRoot(summarizeRootElement);
-
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     window.Word = Word;
-    ReactDOM.render(<Summarize  title={summarizeTitle} />, document.getElementById("summarize-root"));
+    ReactDOM.render(<Summarize title="Propylon Legislation Summarize" />, document.getElementById("summarize-root"));
   }
 });
