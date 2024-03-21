af0f99cc1f0b1fafa4c28fdd8ee40c7871636a8e
Hazel Hilbert <hazelhilbert@gmail.com>
2024-03-21 16:05:51 +0000

    changed tests to mock-requests

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,4 +1,6 @@
 import requests
+import requests_mock
+import pytest
 
 test_url = "http://127.0.0.1:5000/"
 
@@ -8,14 +10,22 @@ def get_comparison_text(filename):
         return file.read()
 
 
-def test_get_bill():
+@pytest.fixture
+def mock_requests():
+    with requests_mock.Mocker() as m:
+        yield m
+
+
+def test_get_bill(mock_requests):
+    mock_requests.get(f"{test_url}billText/1254828", text=get_comparison_text('test1.txt'))
     response = requests.get(f"{test_url}billText/1254828")
     assert str(response) == '<Response [200]>'
     assert response.text == get_comparison_text('test1.txt')
     assert response.text != get_comparison_text('dummyTest.txt')
 
 
-def test_citation_string_bill():
+def test_citation_string_bill(mock_requests):
+    mock_requests.get(f"{test_url}citationStringBill/1254828", text="105 ILCS 5/10-22.6 and 105 ILCS 5/34-19")
     response = requests.get(f"{test_url}citationStringBill/1254828")
     assert response.status_code == 200
     data = response.text
