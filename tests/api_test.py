62d09c1b9ddc85e6156c7d08c0936f0d384526b8
Hazel Hilbert <hazelhilbert@gmail.com>
2024-03-21 15:51:40 +0000

    changed tests from unittest to pytest

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,33 +1,23 @@
-import os
 import requests
-#import app
-#import pytests
 
 test_url = "http://127.0.0.1:5000/"
 
-def getComparisonText(filename):
-    with open('testFiles/' + filename, 'r') as file:
-        data = file.read()
-        return data
 
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-
-    def testGetBill(self):
-        response = requests.get(test_url + "billText/1254828")
-        self.assertEqual(str(response), '<Response [200]>')
-        self.assertEqual(response.text, getComparisonText('test1.txt'))
-        self.assertNotEqual(response.text, getComparisonText('dummyTest.txt'))
+def get_comparison_text(filename):
+    with open(f'tests/testFiles/{filename}', 'r') as file:
+        return file.read()
 
 
-    def testCitationStringBill(self):
-        response = requests.get(test_url + "citationStringBill/1254828")
-        self.assertEqual(response.status_code, 200)
-        data = response.text
-        self.assertIn("105 ILCS 5/10-22.6", data)
-        self.assertIn("105 ILCS 5/34-19", data)
+def test_get_bill():
+    response = requests.get(f"{test_url}billText/1254828")
+    assert str(response) == '<Response [200]>'
+    assert response.text == get_comparison_text('test1.txt')
+    assert response.text != get_comparison_text('dummyTest.txt')
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_citation_string_bill():
+    response = requests.get(f"{test_url}citationStringBill/1254828")
+    assert response.status_code == 200
+    data = response.text
+    assert "105 ILCS 5/10-22.6" in data
+    assert "105 ILCS 5/34-19" in data
