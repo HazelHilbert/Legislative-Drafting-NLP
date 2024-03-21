4fc8df16bd98eb9544d8c5abdd0c29b3349d20c8
Hazel Hilbert <hazelhilbert@gmail.com>
2024-03-21 15:24:35 +0000

    fixed issue with tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ import requests
 test_url = "http://127.0.0.1:5000/"
 
 def getComparisonText(filename):
-    with open('tests/testFiles/' + filename, 'r') as file:
+    with open('testFiles/' + filename, 'r') as file:
         data = file.read()
         return data
 
@@ -17,17 +17,17 @@ class TestStringMethods(unittest.TestCase):
     def testGetBill(self):
         response = requests.get(test_url + "billText/1254828")
         self.assertEqual(str(response), '<Response [200]>')
-        # print(os.getcwd())
         self.assertEqual(response.text, getComparisonText('test1.txt'))
         self.assertNotEqual(response.text, getComparisonText('dummyTest.txt'))
 
 
     def testCitationJSON(self):
-        response = requests.get(test_url + "citationJSONBill/1254828")
+        response = requests.get(test_url + "citationStringBill/1254828")
         self.assertEqual(response.status_code, 200)
-        data = response.json
-        self.assertIn("citations", data)
+        data = response.text
+        self.assertIn("105 ILCS 5/10-22.6", data)
+        self.assertIn("105 ILCS 5/34-19", data)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
