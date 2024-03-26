0b3a82941b64cff3ad354c03a000452f81f7dec5
Mauv7 <125374760+Mauv7@users.noreply.github.com>
2024-03-26 13:03:32 +0000

    Revert "Dev frontend search"

--- frontend_nlp_react/src/taskpane/components/SearchPageComponents/ResultsPage.jsx
+++ frontend_nlp_react/src/taskpane/components/SearchPageComponents/ResultsPage.jsx
@@ -3,25 +3,12 @@ import React from "react";
 import ResultItem from "./ResultItem";
 import "./ResultsPage.css"; // Import the CSS file
 
-const ResultsPage = ({ searchResults }) => {
-  
-  console.log(searchResults);
-  return (
+const ResultsPage = () => (
   <div className="resultsContainer">
-    
-    {/* Check if searchResults is not null or undefined before mapping over it */}
-    {searchResults && searchResults.map((result, index) => (
-      <ResultItem
-        key={index}
-        title={result.title}
-        state={result.state}
-        date={result.last_action_date}
-        url={result.url}
-      />
-    ))}
+    <ResultItem title="Lorem Ipsum" state="TX" date="Jan 12, 2024" />
+    <ResultItem title="Second Result" state="TX" date="Jan 12, 2024" />
+    <Button appearance="primary">Insert 2</Button>
   </div>
-  );
-};
+);
 
 export default ResultsPage;
-
