7950e4a3daa81dddbfcd42fddb847ba2f0620536
Kuriboh-Lord <106563396+Kuriboh-Lord@users.noreply.github.com>
2024-04-05 15:30:40 +0100

    Finalised training functions

--- backend/Training/CreateData.py
+++ backend/Training/CreateData.py
@@ -14,32 +14,55 @@ from datasets import load_dataset
 from torch.utils.data import Dataset
 from transformers import DataCollatorForTokenClassification
 from transformers import BertTokenizer, BertForTokenClassification
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TextClassificationPipeline
+
 
 from transformers import TrainingArguments, Trainer
 
 from huggingface_hub import login
 
 import evaluate
+import csv
+
+
 
 metric = evaluate.load("seqeval")
 
+numTrainingFiles = 0
+target = 1000
+
 print("Start")
 
+csv.field_size_limit(100000000)  # Set to a suitable value depending on your data size
+
+bills = []
+
+# Open csv file with training data
+with open('output.csv', 'r', encoding='utf-8', errors='ignore') as file:
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        newBill = []
+        newBill.append(row[0])
+        newBill.append(row[1].split(','))
+        bills.append(newBill)
+        numTrainingFiles += 1
+        if numTrainingFiles == target:
+            break
+        
+
+# Our labels used by the model
 label_names = ["B-LC", "I-LC", "O"]
 
+# Load in the model and its tokenizer from Hugging Face
 model_name = "kavans25/SwEng25"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = BertForTokenClassification.from_pretrained(model_name, num_labels=3)  # Adjust num_labels as needed
 data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
 
-bills = []
+# Create pipe for testing
+pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores=True)
 
-with open('dummy.jsonl') as f:
-    data = [json.loads(line) for line in f]
-
-
-
+# Declare training arguments
 args = TrainingArguments(
     "kavans25/SwEng25",
     evaluation_strategy="epoch",
@@ -50,6 +73,15 @@ args = TrainingArguments(
     push_to_hub=True,
 )
 
+# Function for testing model
+def testModel():
+    inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+    labels = torch.tensor([10])  # Adjust batch size as needed
+    print(labels)
+    outputs = model(**inputs, labels=labels)
+    print(outputs)
+
+# Class for converting training data to dataset
 class CustomDataset(Dataset):
     def __init__(self, texts, labels, tokenizer):
         self.texts = texts
@@ -71,7 +103,8 @@ class CustomDataset(Dataset):
             "input_ids": tokenized_input["input_ids"],
             "labels": labels
         }
-    
+
+# Function to assist in correctly configuring value arrays
 def alignValArr(valArr):
     label_map = {"B-LC": 1, "I-LC": 2, "O": 0}
     newArr = []
@@ -82,6 +115,7 @@ def alignValArr(valArr):
     print(newArr)
     return newArr
 
+# Function for computing testing metrics
 def compute_metrics(eval_preds):
     logits, labels = eval_preds
     predictions = np.argmax(logits, axis=-1)
@@ -100,52 +134,96 @@ def compute_metrics(eval_preds):
         "accuracy": all_metrics["overall_accuracy"],
     }
 
+# Sample training data
 sample_data = ["the provisions of 12 O.S. 1941 had an impact on 34 N.C. 1234", ["34 N.C. 1234","12 O.S. 1941"]]
 eval_data = ["the understanding of 43 N.C. 1957 helped improve the legislation of 67 N.Y. 3456", ["43 N.C. 1957","67 N.Y. 3456"]]
 
+# Training function
 def train(sample_data, eval_data):
+    # Training data text
     text =  sample_data[0]
+    # Training data citations
     test_citations = sample_data[1]
 
+    # Convert text to tokens with isolated citations
     tokens = tokenize(text, test_citations)
-    
+
+    #Create and align label array
     valArrS = createValArr(tokens, test_citations)
     valArr = alignValArr(valArrS)
-    tokens = tokenizer.tokenize(text)
-    print(tokens)
 
-    # Prepare your dataset here
-    texts = [tokens]  # Your texts
-    labels = [valArr]  # Your labels, aligned and adjusted as necessary
-    dataset = CustomDataset(texts, labels, tokenizer)
-    print(dataset)
+    # Tokenize text without isolating citations
+    tokens = tokenizer.tokenize(text)
+
+    # Eval data text
+    eval_text = eval_data[0]
+    # Eval data citations
+    eval_test_citations = eval_data[1]
+
+    # Convert eval text to tokens with isolated citations
+    eval_tokens = tokenize(eval_text, eval_test_citations)
+
+    #Create and align eval label array
+    eval_valArrS = createValArr(eval_tokens, eval_test_citations)
+    eval_valArr = alignValArr(eval_valArrS)
+
+    # Tokenize eval text without isolating citations
+    eval_tokens = tokenizer.tokenize(eval_text)
+
+    # Truncate if either array is too large, otherwise will not be accepted
+    if len(tokens) > 510:
+        tokens = tokens[0:510]
+        valArr = valArr[0:509]
+        valArr.append(-100)
+
+    if len(eval_tokens) > 510:
+        eval_tokens = eval_tokens[0:510]
+        eval_valArr = eval_valArr[0:509]
+        eval_valArr.append(-100)
+
+    # Align length of arrays and label arrays
+    if len(tokens) > len(eval_tokens):
+        append_len = len(tokens) - len(eval_tokens)
+        print(f"Append_len: {append_len}")
+        for i in range(append_len):
+            eval_tokens.append('O')
+
+    if len(tokens) < len(eval_tokens):
+        append_len = len(eval_tokens) - len(tokens)
+        print(f"Append_len: {append_len}")
+        for i in range(append_len):
+            tokens.append('O')
+
+    if len(valArr) > len(eval_valArr):
+        append_len = len(valArr) - len(eval_valArr)
+        print(f"Append_len: {append_len}")
+        for i in range(append_len):
+            eval_valArr.append(-100)
+
+    if len(valArr) < len(eval_valArr):
+        append_len = len(eval_valArr) - len(valArr)
+        print(f"Append_len: {append_len}")
+        for i in range(append_len):
+            valArr.append(-100)
 
     
+    # Prepare dataset here
+    texts = [tokens]  # Texts
+    labels = [valArr]  # Labels, aligned and adjusted as necessary
+    # Create eval dataset
+    dataset = CustomDataset(texts, labels, tokenizer)
 
-    batch = data_collator(dataset)
-    print(batch["labels"])
-
+    # Use predictions to compute metrics
     predictions = valArrS.copy()
     predictions[2] = 'O'
     metric.compute(predictions=[predictions], references=[valArrS])
 
-    eval_text = eval_data[0]
-    eval_test_citations = eval_data[1]
-
-    eval_tokens = tokenize(eval_text, eval_test_citations)
-
-    eval_valArrS = createValArr(eval_tokens, eval_test_citations)
-    eval_valArr = alignValArr(eval_valArrS)
-    eval_tokens = tokenizer.tokenize(eval_text)
-    print(eval_tokens)
-
-    eval_texts = [eval_tokens]  # Your texts
-    eval_labels = [eval_valArr]  # Your labels, aligned and adjusted as necessary
+    eval_texts = [eval_tokens]  # Texts
+    eval_labels = [eval_valArr]  # Labels, aligned and adjusted as necessary
+    # Create eval dataset
     eval_dataset = CustomDataset(eval_texts, eval_labels, tokenizer)
-    print(eval_dataset)
-
-  
-    # Then, create your Trainer with this dataset
+    
+    # Create Trainer with this dataset
     trainer = Trainer(
         model=model,
         args=args,
@@ -157,22 +235,6 @@ def train(sample_data, eval_data):
     )
 
     trainer.train()
-    #tokens = tokenizer(tokens, is_split_into_words=True)
-    #print(f"tokenized text: {tokenizer(text)}")
-    #print(tokens)
-    #print(valArr)
-    #print(tokens["token_type_ids"])
-    #print(tokens.tokens())
-    #tokens["token_type_ids"] = alignValArr(valArr)
-    #print(tokens)
-
-    #trainer = Trainer(
-    #    model=model,
-    #    args=args,
-    #    train_dataset=tokens,
-    #    tokenizer=tokenizer,
-    #)
-    #trainer.train()
 
 
 
@@ -222,6 +284,8 @@ def evaluate_model():
     for citation in citations:
         print(citation)
 
+# This function takes in a tokenized array and it's citations and returns an
+#   array of corresponding labels.
 def createValArr(arr, cite):
     valArr = []
     for i in arr:
@@ -237,6 +301,7 @@ def createValArr(arr, cite):
     print(valArr)
     return valArr
 
+# Utility function to compare 2 arrays. Returns True if they are the same
 def CompareArray(arrayA, arrayB):
     if len(arrayA) == len(arrayB):
         for i in range(len(arrayA)):
@@ -245,39 +310,29 @@ def CompareArray(arrayA, arrayB):
         return True
     return False
 
-trackCurrent = 0
-trackTotal = len(data)
+# Separate test Function
+def testModel():
+    input_text = "the Germany of 12 O.S. 1941 had an impact on 34 N.C. 1234"
 
-def ConvertToTraining(bill):
-    valArr = createValArr(tokenize(bill[0], bill[1]), bill[1])
-    print(valArr)
-    tokens = tokenizer.tokenize(text)
-    print(tokens)
-    return [tokens, valArr]
-    
+    inputs = tokenizer(input_text, return_tensors="pt")
 
-def Create(data):
-    new_bill = []
-    bill_cites = []
-    bill_id = str(data['id'])
-    stri = ''
-    stri += (data['casebody']['data']['head_matter'])
-    stri += (data['casebody']['data']['corrections'])
-    stri += (data['casebody']['data']['opinions'][0]['text'])
-    new_bill.append(stri)
-    api_url = "https://api.case.law/v1/cases/" + bill_id
-    response = requests.get(api_url)
-    data = response.json()
-    citations = (data['cites_to'])
-    bill_cites = []
-    for i in citations:
-        bill_cites.append(i['cite'])
-    new_bill.append(bill_cites)
-    bills.append(new_bill)
+    # Generate predictions
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
 
-for i in data:
-    Create(i)
+    # Get predicted labels
+    predicted_labels = torch.argmax(logits, dim=2)
 
+    # Convert predicted labels back to named entities
+    # Here you would need to map the predicted label IDs to the corresponding labels in your NER task
+    predicted_entities = [tokenizer.decode(label) for label in predicted_labels[0]]
+
+    # Print the input text with predicted entities
+    for token, entity in zip(tokenizer.tokenize(input_text), predicted_entities):
+        print(f"{token}\t{entity}")
+
+# Function to tokenize while isolating citations as individual tokens
 def tokenize(text, citations_list):
     citations = []
     
@@ -303,5 +358,9 @@ def tokenize(text, citations_list):
     print(tokens)
     return tokens
 
-train(sample_data, eval_data)
-
+# Function to run training
+def runTraining():
+    i = 0
+    while i < (len(bills) - 2):
+        train(bills[i], bills[i+1])
+        i += 1
