fce06baf7c2403169480033bafbce83f0564906f
roscanTCD <127952096+roscanTCD@users.noreply.github.com>
2024-04-06 08:09:27 +0100

    Configured webpack.config.js file to run multiple runtime loaders

--- frontend_nlp_react/webpack.config.js
+++ frontend_nlp_react/webpack.config.js
@@ -100,12 +100,7 @@ module.exports = async (env, options) => {
       new HtmlWebpackPlugin({
         filename: "taskpane.html",
         template: "./src/taskpane/taskpane.html",
-        chunks: ["taskpane", "vendor", "polyfill"],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["commands"],
+        chunks: ["polyfill", "taskpane", "commands", "functions", "vendor"],
       }),
 
       new HtmlWebpackPlugin({
